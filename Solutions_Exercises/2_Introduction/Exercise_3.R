84fdbbb5cd9bc4df8c7652f0c87485578648a293
jdegenfellner <jdegenfellner@gmail.com>
2025-01-17 10:23:43 +0100

    up

--- Solutions_Exercises/2_Introduction/Exercise_3.R
+++ Solutions_Exercises/2_Introduction/Exercise_3.R
@@ -1,8 +1,21 @@
-library(rethinking)
-data("Howell1")
-d <- Howell1
-str(d)
-d2 <- d[d$age >= 18, ]
-mod <- lm(height ~ 1, data=d2)
-sqrt(sum(mod$residuals^2) / (nrow(d2) - 1))
-sd(d2$height)
+library(DiagrammeR)
+
+grViz("
+digraph model {
+  graph [layout = dot, rankdir = TB]
+
+  # Nodes for variables
+  node [shape = ellipse, style = filled, fillcolor = lightblue]
+  alpha [label = 'α ~ Normal(171.1, 20)']
+  beta [label = 'β ~ Normal(0, 10)']
+  sigma [label = 'σ ~ Uniform(0, 50)']
+  mu [label = 'μ_i = α + β(x_i - x̄)']
+  h_i [label = 'h[i] ~ Normal(μ_i, σ)']
+
+  # Connections
+  alpha -> mu
+  beta -> mu
+  mu -> h_i
+  sigma -> h_i
+}
+")
