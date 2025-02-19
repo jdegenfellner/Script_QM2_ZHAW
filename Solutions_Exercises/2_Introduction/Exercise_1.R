0510d75b1a5e1c8f7f81f392046c84fd6438400b
jdegenfellner <jdegenfellner@gmail.com>
2025-02-19 11:58:26 +0100

    up

--- Solutions_Exercises/2_Introduction/Exercise_1.R
+++ Solutions_Exercises/2_Introduction/Exercise_1.R
@@ -1,6 +1,10 @@
 # mean
-heights_CH_mean <-  171.1
-n_CH <-  21873
+heights_CH_mean <-  171.1 # cm
+n_CH <-  21873 # sample size
+
+# ZGV:
+# CI for the true, but unknown mean of the population is:
+# \bar{x} \pm 1.96 * \frac{s}{\sqrt{n}}
 
 # width CI:
 c(-0.002*171.1,0.002*171.1) # 0.2%
@@ -14,7 +18,7 @@ heights_CH_mean + c(-0.002*171.1,0.002*171.1)
 # the last part = 0.2
 
 # Hence, sd(heights_CH):
-# 0.2 = 1.96 *sd/sqrt(n_CH)
+# 0.2*171.1 = 1.96 *sd/sqrt(n_CH)
 
 # sd = 
 0.002*heights_CH_mean * sqrt(n_CH)/1.96
