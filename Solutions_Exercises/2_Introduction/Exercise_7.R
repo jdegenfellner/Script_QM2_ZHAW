60c03641e41b91293e05a98f57fad768272fd97d
jdegenfellner <jdegenfellner@gmail.com>
2025-02-19 18:35:23 +0100

    up

--- Solutions_Exercises/2_Introduction/Exercise_7.R
+++ Solutions_Exercises/2_Introduction/Exercise_7.R
@@ -1,5 +1,11 @@
 # Define the covariance matrix
 sigma <- matrix(c(0.75, 0.5, 0.5, 0.75), ncol = 2)
+sigma # Variance covariance matrix
+# 0.75 = variance of X
+# 0.75 = variance of Y
+# 0.5 = covariance between X and Y
+# see: https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Bivariate_case
+
 
 # Extract variances (diagonal elements)
 var_x <- sigma[1, 1]
@@ -17,20 +23,7 @@ cor_xy <- cov_xy / (sd_x * sd_y)
 
 # Construct correlation matrix manually
 cor_matrix <- matrix(c(1, cor_xy, cor_xy, 1), ncol = 2)
-
-# Print results
-print("Variance of X:")
-print(var_x)
-print("Variance of Y:")
-print(var_y)
-print("Covariance between X and Y:")
-print(cov_xy)
-print("Standard Deviation of X:")
-print(sd_x)
-print("Standard Deviation of Y:")
-print(sd_y)
-print("Correlation Matrix:")
-print(cor_matrix)
+cor_matrix
 
 # Verify with built-in function
 print("Correlation Matrix using cov2cor():")
