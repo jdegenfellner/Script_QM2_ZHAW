0a7d5ea0a1031db92b0674d4e9bf5b64e2f8d9f3
jdegenfellner <jdegenfellner@gmail.com>
2025-02-21 12:38:05 +0100

    up

--- Solutions_Exercises/2_Introduction/Exercise_1.R
+++ Solutions_Exercises/2_Introduction/Exercise_1.R
@@ -15,10 +15,10 @@ heights_CH_mean + c(-0.002*171.1,0.002*171.1)
 
 # CI was created with the following formula:
 # mean(heights_CH) + c(-1.96,1.96) * sd(heights_CH) / sqrt(n_CH)
-# the last part = 0.2
+# the last part = 0.002
 
 # Hence, sd(heights_CH):
-# 0.2*171.1 = 1.96 *sd/sqrt(n_CH)
+# 0.002*171.1 = 1.96 *sd/sqrt(n_CH)
 
 # sd = 
 0.002*heights_CH_mean * sqrt(n_CH)/1.96
