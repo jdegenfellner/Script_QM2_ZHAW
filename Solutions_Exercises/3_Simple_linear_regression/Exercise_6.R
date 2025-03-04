86e36553bc183c8f009af043b9d329b1393cd24a
jdegenfellner <jdegenfellner@gmail.com>
2025-03-04 15:23:41 +0100

    up

---
+++ Solutions_Exercises/3_Simple_linear_regression/Exercise_6.R
@@ -0,0 +1,23 @@
+set.seed(123)
+
+# Generate data from a sine function
+x <- seq(0, 2 * pi, length.out = 100)       # Predictor variable
+true_y <- sin(x)                            # True values based on sine function
+
+plot(x, true_y, type = "l", col = "blue", lwd = 2, 
+     xlab = "x", ylab = "y", main = "True Sine Function")
+
+# Add noise to simulate observed data
+noise <- rnorm(length(x), mean = 0, sd = 0.2)
+observed_y <- true_y + noise                # Observed response variable
+
+points(x, observed_y, col = "red", pch = 19)
+
+# Perform a simple linear regression with intercept
+linear_model <- lm(observed_y ~ x)
+
+# Display summary of the model
+#summary(linear_model)
+
+# add regression line 
+abline(linear_model, col = "green", lwd = 2)
